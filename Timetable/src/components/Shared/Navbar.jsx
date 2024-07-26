ec9bb2380d87c5a0572e0b0f661ba19328678f3d
Subhalakshmi S <subhalakshmi279@gmail.com>
2024-07-26 07:08:06 +0530

    new change

---
+++ Timetable/src/components/Shared/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+// import { ModeToggle } from '../mode-toggle'
+
+const Navbar = () => {
+  const NavLinks = [
+    {
+      title: "Home",
+      path: "/"
+    },
+    {
+      title: "Login",
+      path: "/login"
+    },
+    {
+      title: "Register",
+      path: "/register"
+    }
+  ]
+  return (
+    <div className="absolute top-0 left-0 w-full h-[8vh] flex flex-row justify-center items-center shadow-sm shadow-primary/50">
+      <div className="w-full h-full text-primary font-bold flex justify-start items-center text-lg ml-5 text-gray-700"> Timetable</div>
+      <div className='h-full font-bold flex flex-row justify-end items-center gap-8 mr-5'>
+
+        {
+          NavLinks.map((links, index) => (
+            <li key={index} className='list-none'>
+              <NavLink to={links.path}>
+                {links.title}
+              </NavLink>
+            </li>
+          ))
+        }
+        {/* <ModeToggle /> */}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ Timetable/src/pages/Shared/Login.jsx
@@ -0,0 +1,45 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import {Link} from 'react-router-dom'
+
+const Login = () => {
+    return (
+        
+        <div className='h-full w-full flex justify-center items-center'>
+            <Card className="w-1/4">
+                <CardHeader className="space-y-1">
+                    <CardTitle className="text-2xl">Login</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="grid gap-4">
+                    <div className="relative">
+                        <div className="absolute inset-0 flex items-center">
+                            <span className="w-full border-t" />
+                        </div>
+                    </div>
+                    <div className="grid gap-2">
+                        <Label htmlFor="email">Email</Label>
+                        <Input id="email" type="email" placeholder="user@iamneo.ai" />
+                    </div>
+                    <div className="grid gap-2">
+                        <Label htmlFor="password">Password</Label>
+                        <Input id="password" type="password" placeholder="******" />
+                    </div>
+                </CardContent>
+                <CardFooter>
+                    <Button className="w-full">Login</Button>
+                </CardFooter>
+                <div className='login'>
+                    <p style={{color:'grey',marginLeft:'10px'}}>Don't have an account ?
+                    <Link to='/Register' style={{color:'darkblue'}}> Register</Link></p>
+                </div>
+            </Card>
+        </div>
+    )
+}
+
+export default Login
